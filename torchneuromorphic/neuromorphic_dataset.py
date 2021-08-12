f794fe7f2bce68e90117742e7e9b0c9184ea88a7
Sami Barchid <sami.barchid@univ-lille.fr>
2021-08-12 11:31:26 +0200

    add torchneuromorphic lib

---
+++ torchneuromorphic/neuromorphic_dataset.py
@@ -0,0 +1,224 @@
+#!/bin/python
+#-----------------------------------------------------------------------------
+# File Name : neuromorphic_dataset.py
+# Author: Emre Neftci
+#
+# Creation Date : Thu Nov  7 11:50:48 2019
+# Last Modified : 
+#
+# Copyright : (c) UC Regents, Emre Neftci
+# Licence : GPLv2
+#----------------------------------------------------------------------------- 
+import os
+import torch
+import torch.utils.data as data
+from torchvision.datasets.utils import extract_archive, verify_str_arg, check_integrity, gen_bar_updater
+from .transforms import Compose
+
+DEFAULT_ROOT = 'data/'
+
+def download_url(url, root, filename=None, md5=None):
+    """Download a file from a url and place it in root.
+    Args:
+        url (str): URL to download file from
+        root (str): Directory to place downloaded file in
+        filename (str, optional): Name to save the file under. If None, use the basename of the URL
+        md5 (str, optional): MD5 checksum of the download. If None, do not check
+    """
+    from six.moves import urllib
+
+    root = os.path.expanduser(root)
+    if not filename:
+        filename = os.path.basename(url)
+    fpath = os.path.join(root, filename)
+
+
+    os.makedirs(root, exist_ok=True)
+
+    # check if file is already present locally
+    if check_integrity(fpath, md5):
+        print('Using downloaded and verified file: ' + fpath)
+    else:   # download the file
+        try:
+            if 'dropbox' in url:
+                # Handle dropbox links differently
+                import requests
+                headers = {'user-agent': 'Wget/1.16 (linux-gnu)'}
+                r = requests.get(url, stream=True, headers=headers)
+                with open(fpath, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=1024): 
+                        if chunk:
+                            f.write(chunk)
+            elif 'Manual' in url:
+                raise urllib.error.URLError(url)
+            else:
+                print('Downloading ' + url + ' to ' + fpath)
+                urllib.request.urlretrieve(
+                    url, fpath,
+                    reporthook=gen_bar_updater()
+                )
+        except (urllib.error.URLError, IOError) as e:
+            if url[:5] == 'https':
+                url = url.replace('https:', 'http:')
+                print('Failed download. Trying https -> http instead.'
+                      ' Downloading ' + url + ' to ' + fpath)
+                urllib.request.urlretrieve(
+                    url, fpath,
+                    reporthook=gen_bar_updater()
+                )
+            else:
+                raise e
+        # check integrity of downloaded file
+        if not check_integrity(fpath, md5):
+            raise RuntimeError("File not found or corrupted.")
+
+def download_and_extract_archive(url, download_root, extract_root=None, filename=None,
+                                 md5=None, remove_finished=False):
+    download_root = os.path.expanduser(download_root)
+    if extract_root is None:
+        extract_root = download_root
+    if not filename:
+        filename = os.path.basename(url)
+
+    download_url(url, download_root, filename, md5)
+
+    archive = os.path.join(download_root, filename)
+    print("Extracting {} to {}".format(archive, extract_root))
+    extract_archive(archive, extract_root, remove_finished)
+
+def identity(x):
+    return x
+
+#TODO make it a hdf5 dataset, implementing generic class NeuromorphicDataset
+class NeuromorphicDataset(data.Dataset):
+    _repr_indent = 4
+
+    def __init__(self, root=None, transforms = None, transform=None, target_transform=None):
+        if isinstance(root, torch._six.string_classes):
+            root = os.path.expanduser(root)
+        self.root = root
+ 
+        if root is not None:
+            if not os.path.isfile(root):
+                if self.download_and_create:
+                    self.download()
+                    self.create_hdf5()
+                else:
+                    raise Exception("File {} does not exist and download_and_create is False".format(root))
+
+
+
+
+        has_transforms = transforms is not None
+        has_separate_transform = transform is not None or target_transform is not None
+        if has_transforms and has_separate_transform:
+            raise ValueError("Only transforms or transform/target_transform can "
+                             "be passed as argument")
+
+        # for backwards-compatibility
+        if transform is None:
+            transform = identity
+        if target_transform is None:
+            target_transform = identity
+
+        self.transform = transform
+        self.target_transform = target_transform
+
+        if has_separate_transform:
+            transforms = StandardTransform(transform, target_transform)
+        self.transforms = transforms
+
+    def __getitem__(self, index):
+        raise NotImplementedError
+
+    def __len__(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        head = "Dataset " + self.__class__.__name__
+        body = ["Number of datapoints: {}".format(self.__len__())]
+        if self.root is not None:
+            body.append("Root location: {}".format(self.root))
+        body += self.extra_repr().splitlines()
+        if hasattr(self, "transforms") and self.transforms is not None:
+            body += [repr(self.transforms)]
+        lines = [head] + [" " * self._repr_indent + line for line in body]
+        return '\n'.join(lines)
+
+    def _format_transform_repr(self, transform, head):
+        lines = transform.__repr__().splitlines()
+        return (["{}{}".format(head, lines[0])] +
+                ["{}{}".format(" " * len(head), line) for line in lines[1:]])
+
+    def extra_repr(self):
+        return ""
+
+    def check_exists(self):
+        res_ = [os.path.exists(d) for d in self.resources_local]
+        res = all(res_)
+        if res is False:
+            print('The following files did not exist, will attempt download:')
+            for i,r in enumerate(res_):
+                if not r: print(self.resources_local[i])
+        return res
+
+
+    def download(self):
+        if self.check_exists():
+            return True
+        else:
+            os.makedirs(self.directory, exist_ok=True)
+            for url, md5, filename in self.resources_url:
+                download_and_extract_archive(url, download_root=self.directory, filename=filename, md5=md5)
+            return False
+
+    def create_hdf5(self):
+        raise NotImplementedError()
+
+    def target_transform_append(self, transform):
+        if transform is None:
+            return
+        if self.target_transform is None:
+            self.target_transform = transform
+        else:
+            self.target_transform = Compose([self.target_transform, transform])
+
+
+    def transform_append(self, transform):
+        if transform is None:
+            return
+        if self.transform is None:
+            self.transform = transform
+        else:
+            self.transform = Compose([self.transform, transform])
+
+
+
+
+class StandardTransform(object):
+    def __init__(self, transform=None, target_transform=None):
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __call__(self, input, target):
+        if self.transform is not None:
+            input = self.transform(input)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+        return input, target
+
+    def _format_transform_repr(self, transform, head):
+        lines = transform.__repr__().splitlines()
+        return (["{}{}".format(head, lines[0])] +
+                ["{}{}".format(" " * len(head), line) for line in lines[1:]])
+
+    def __repr__(self):
+        body = [self.__class__.__name__]
+        if self.transform is not None:
+            body += self._format_transform_repr(self.transform,
+                                                "Transform: ")
+        if self.target_transform is not None:
+            body += self._format_transform_repr(self.target_transform,
+                                                "Target transform: ")
+
+        return '\n'.join(body)
