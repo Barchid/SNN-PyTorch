a3f145148c4f26d83bf96a7978377dbc06784285
Sami Barchid <sami.barchid@univ-lille.fr>
2021-08-30 17:01:39 +0200

    implemented ttfs, phase coding and tested

--- neural_coding.py
+++ neural_coding.py
@@ -8,6 +8,31 @@ def rate_coding(image: np.ndarray):
     pass
 
 
+def phase_coding(image: np.ndarray, timesteps: int = 100, is_weighted: bool = True):
+    # asserts
+    assert len(image.shape) == 2  # force grayscale
+    # check the conversion is possible. 8 because this will be the number of phases for the bit representation of [0, 255]
+    assert timesteps % 8 == 0
+
+    # compute number of periods
+    periods = timesteps // 8
+
+    # binary representation of the image (it makes 8 )
+    bit_representation = np.unpackbits(
+        image[..., None], axis=-1).transpose(2, 0, 1)
+
+    # IF the weighted input option is used
+    if is_weighted:
+        # obtained with the equation seen in the referenced paper
+        w_s = [0.5, 0.25, 0.125, 0.0625, 0.0313, 0.015625, 0.0078125, 0.00390625]
+        # TODO
+
+    # Repeat the bit representation to create the final output spikes
+    S = np.tile(bit_representation, periods)
+
+    return S
+
+
 def P_th(t: float, theta_0: float, tau_th: float):
     # calculate Pth(t)
     P_th = theta_0 * math.exp(-t/tau_th)
@@ -15,6 +40,18 @@ def P_th(t: float, theta_0: float, tau_th: float):
 
 
 def ttfs(image: np.ndarray, theta_0: float = 1.0, tau_th: float = 6.0, timesteps: int = 300):
+    """Implementation of Time-To-First Spike (TTFS) neural coding for a grayscale image, as defined in
+    the paper : https://www.frontiersin.org/articles/10.3389/fnins.2021.638474/full#F2
+
+    Args:
+        image (np.ndarray): the grayscale image of dimension (H, W) to convert into spikes
+        theta_0 (float, optional): the parameter theta_0 (see the paper). Defaults to 1.0.
+        tau_th (float, optional): the time constant parameter to compute the threshold at each timestep (see the paper). Defaults to 6.0.
+        timesteps (int, optional): the total number of timesteps for one image. Defaults to 300.
+
+    Returns:
+        np.ndarray: the neural encoded image of dimension (timesteps, H, W)
+    """
     # asserts
     assert len(image.shape) == 2  # force grayscale
     assert tau_th > 0.
@@ -47,6 +84,12 @@ if __name__ == '__main__':
     ts = 100
 
     image = cv2.imread('input.jpg', cv2.IMREAD_GRAYSCALE)
+    print(image.shape)
+
+    a = np.tile(image[..., None], 3)
+    print(a.shape)
+
+    exit()
     S = ttfs(image, theta_0, tau_th, ts)
     print(S.shape, np.unique(S))
     spikes = (S == 1.).sum()
@@ -57,3 +100,7 @@ if __name__ == '__main__':
     print(333 * 500 * 100 == spikes + nonspikes)
 
     print(333 * 500, spikes, abs(333 * 500 - spikes))
+
+    print('Spikes !!!!!')
+    for t in range(ts):
+        print((S[t] == 1.).sum())
